2c0cb35b64e6f137c468234319af11fdbc45e414
STEAL <stealinp@gmail.com>
2026-05-15 10:44:19 +0800

    q5

--- pyq24.cpp
+++ pyq24.cpp
@@ -1,51 +1,46 @@
 #include <iostream>
 
-int main(){
-	int A[10]={0,1,2,3,4,5,6,7,8,9};
-	int B[18]={0,-1,-2,-3,-4,-5,-6,-7,-8,
-		-9,-10,-11,-12,-13,-14,-15,-16,-17};
-	int C[8]={0,1,2,3,4,5,6,7};
-	int i,j,temp;
-
-	for(i=0;i<10;i++){
-		std::cout<<A[i]<<"\n";
-	}
-	for(i=0;i<18;i++){
-		std::cout<<B[i]<<"\n";
-	}
-	for(i=0;i<8;i++){
-		std::cout<<C[i]<<"\n";
-	}
-	for(i=1;i<=8;i=i+2){
-		for(j=0;j<2;j++){
-			temp=C[i-j];
-			C[i-j]=B[i];
-			B[i]=temp;
-
-			temp=C[i];
-			C[i]=B[i-j];
-			B[i-j]=temp;
-		} // Constant time, will only run 2 time 
-	} //Linear time, will run n/2 time
-	temp=A[0];
-	for(i=0;i<9;i++){
-		A[i]=A[i+1];
-	}
-	A[9]=B[17];
-	for(i=17;i>9;i=i-2){
-		B[i]=B[i-2];
-	}
-	B[9]=temp;
-	for(i=0;i<10;i++){
-		std::cout<<A[i]<<"\n";
-	}
-	for(i=0;i<18;i++){
-		std::cout<<B[i]<<"\n";
-	}
-	for(i=0;i<8;i++){
-		std::cout<<C[i]<<"\n";
-	}
+	//[0,1,2,3,4,5,6] 
+	//   v       ^
+	//[0,1,2,3,4,5,6] 
+void circle(int A[],int B[],const int sizes){
+	int temp,i,j;
+	if(sizes%2==0) i=sizes-1;
+	else i=sizes-2;
+	temp=A[1];
+	for(j=1;j<sizes;j=j+2){
+		if(j+2>=sizes) break;
+		A[j]=A[j+2];
+	} //move odd indexes to the left
+	A[i]=B[i];
+	for(j=i;j>0;j=j-2){
+		if(j-2<0) break;
+		B[j]=B[j-2];
+	} //move odd indexes to the right
+	B[1]=temp;
 
 }
 
-//this program is O(N)
+int main(){
+	const int sizes=7;
+	int A[sizes];
+	int B[sizes];
+	
+
+	int i;
+
+	for(i=0;i<sizes;i++){
+		A[i]=i;
+		B[i]=i;
+	}
+	for(i=0;i<6;i++){
+		circle(A,B,sizes);
+	} //do 6 times
+	for(i=0;i<sizes;i++){
+		std::cout<<A[i]<<" ";
+	}
+	std::cout<<"\n";
+	for(i=0;i<sizes;i++){
+		std::cout<<B[i]<<" ";
+	}
+}
